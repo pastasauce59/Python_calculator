3699ff282e581763ece3cad360c71cb64361b456
Michael Primavera <pastasauce59@gmail.com>
2023-02-16 23:58:59 -0500

    Created a 'calculation' function to house the entire logic for the calculator and changed the 'keep_going' input to ask if the user wants to start over, where calculation function will call itself with recursion.

--- python_calc.py
+++ python_calc.py
@@ -20,20 +20,24 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-continue_ = True
-while continue_:
-    operation_symbol = input("Pick an operation: ")
-    num2 = int(input("What's the next number?: "))
-    calculations_function = operations[operation_symbol]
-    answer = calculations_function(num1, num2)
-    
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    
-    keep_going = input(f"Type \"y\" to continue calculating with {answer}, or type \"n\" to exit.: ")
-    if keep_going != "y":
-        continue_ = False
-    else:
-        num1 = answer
+def calculation():
+    num1 = int(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    continue_ = True
+    while continue_:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calculations_function = operations[operation_symbol]
+        answer = calculations_function(num1, num2)
+        
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        keep_going = input(f"Type \"y\" to continue calculating with {answer}, or type \"n\" to start a new calculation.: ")
+        if keep_going != "y":
+            continue_ = False
+            calculation()
+        else:
+            num1 = answer
+
+calculation()
