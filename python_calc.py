c0f2978cb1606d5978c4a66f24b6c5b99a3076e4
Michael Primavera <pastasauce59@gmail.com>
2023-02-16 23:51:39 -0500

    created a while loop and input prompt to allow the user to continue doing calculations with the previous answer or to exit the application.

--- python_calc.py
+++ python_calc.py
@@ -23,12 +23,17 @@ operations = {
 num1 = int(input("What's the first number?: "))
 for symbol in operations:
     print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculations_function = operations[operation_symbol]
-answer = calculations_function(num1, num2)
-# for symbol in operations:
-#     if symbol == operation_symbol:
-#         answer = operations[symbol](num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+continue_ = True
+while continue_:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number?: "))
+    calculations_function = operations[operation_symbol]
+    answer = calculations_function(num1, num2)
+    
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
+    keep_going = input(f"Type \"y\" to continue calculating with {answer}, or type \"n\" to exit.: ")
+    if keep_going != "y":
+        continue_ = False
+    else:
+        num1 = answer
